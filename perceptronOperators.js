e8a491835494c69981217917594a60e3f9ea695a
Nolan <nolan.donb@gmail.com>
2022-06-04 16:34:22 -0600

    Add AND / OR Perceptrons

---
+++ perceptronOperators.js
@@ -0,0 +1,44 @@
+// AND PERCEPTRON
+
+let testExamples = {
+    1: [0,0], // false
+    2: [0,1], // false
+    3: [1,0], // false
+    4: [1,1]  // true
+};
+
+let weight1 = 1;
+let weight2 = 1;
+let bias = -1.5;
+
+const prediction = (result) => {
+    console.log(result)
+    console.log(result >= 0)
+}
+
+for (const arr in testExamples) {
+    prediction((testExamples[arr][0] * weight1) + (testExamples[arr][1] * weight2) + bias)
+}
+
+
+// OR PERCEPTRON
+
+let testExamples1 = {
+    1: [0,0], // false
+    2: [0,1], // true
+    3: [1,0], // true
+    4: [1,1]  // true
+};
+
+let testExamples1Weight1 = 10;
+let testExamples1Weight2 = 10;
+let testExamples1Bias = 0;
+
+
+console.log("---- DONE PREDICTION -----")
+
+for (const arr in testExamples1) {
+    prediction((testExamples1[arr][0] * testExamples1Weight1) + (testExamples1[arr][1] * testExamples1Weight2) + testExamples1Bias)
+}
+
+console.log("---- DONE PREDICTION -----")
